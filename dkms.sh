b0d1b31da205886a2c82a161194c3eeaf04bf2e8
Iwushik <iwushik@gmail.com>
2021-04-21 15:21:54 +0200

    Improved scripts

---
+++ dkms.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ "$1" = "-u" ]; then
+    dkms remove hid-asus-flow-x13/1.0 --all
+else
+    dkms install drivers/hid
+fi
+
+dracut --force
---
+++ udev.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ "$1" = "-u" ]; then
+	rm /etc/udev/rules.d/99-asus-flow-power.rules
+	rm /etc/udev/rules.d/99-asus-flow-power.rules
+else
+	mkdir -p /etc/udev/rules.d
+
+	cat <<- EOF > /etc/udev/rules.d/99-asus-flow-power.rules
+	# nvidia dGPU
+	ACTION=="add", SUBSYSTEM=="pci", TEST=="power/control", ATTR{vendor}=="0x10de", ATTR{power/control}="auto"
+	EOF
+	
+	cat <<- EOF > /etc/udev/rules.d/99-asus-flow-power.rules
+	# nvme ssd
+	ACTION=="add", SUBSYSTEM=="pci", DRIVER=="nvme", TEST=="power/control", ATTR{power/control}="auto"
+	EOF
+	
+	udevadm control --reload
+fi
