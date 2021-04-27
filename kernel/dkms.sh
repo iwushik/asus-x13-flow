357cb5a2f593b9df844ee8ce6c3f810532e9ce35
TheEdward162 <theedward162@gmail.com>
2021-04-27 18:29:58 +0200

    kernel: update dkms script

--- kernel/dkms.sh
+++ kernel/dkms.sh
@@ -5,9 +5,11 @@ set -e
 if [ "$1" = "-u" ]; then
     dkms remove asus-flow-x13/1.0 --all
 else
+    rm -rf sound
     cp -r upstream/sound sound
     git apply patches/sound.patch
     
+    rm -rf drivers
     cp -r upstream/drivers drivers
     git apply patches/keyboard.patch
 
